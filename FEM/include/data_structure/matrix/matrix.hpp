248950fc046af1fb14a4fa3e98b1794ac06ba0cf
Phuong0407 <diepthanhphuong0407@gmail.com>
2025-02-19 22:24:57 +0100

    add file for matrix

---
+++ FEM/include/data_structure/matrix/matrix.hpp
@@ -0,0 +1,13 @@
+#ifndef DATA_STRUCTURE_MATRIX_MATRIX_HPP
+#define DATA_STRUCTURE_MATRIX_MATRIX_HPP
+
+#include <data_structure/tensor/tensor.hpp>
+
+namespace data_structure
+{
+    
+    class matrix {};
+
+} // namespace data_structure
+
+#endif
